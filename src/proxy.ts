3e3ac5a6ec71ee25830fe4c22922ee36593430e6
Thalis alves <diretoriaensino@husf.org.br>
2026-05-19 23:51:14 -0300

    remove middleware and keep next proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,3 +10,5 @@ export const config = {
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"
   ]
 };
+
+//so para testar o proxy, nao precisa disso, mas se quiser usar o proxy para outras coisas, pode usar esse middleware para atualizar a sessão do supabase em todas as rotas, exceto as que estao na lista de excecoes acima.
